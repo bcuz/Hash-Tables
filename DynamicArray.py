65121cab21cc73da6de981cce1155924d171aaa4
adam <adam.nawroc@gmail.com>
2020-01-29 21:29:34 -0500

    fuck 4 spaces

--- DynamicArray.py
+++ DynamicArray.py
@@ -1,43 +1,43 @@
 class DynamicArray:
-    def __init__(self, size):
-        self.count = 0
-        self.size = size
+  def __init__(self, size):
+    self.count = 0
+    self.size = size
 
-        self.storage = [None] * size
+    self.storage = [None] * size
 
-     # [None, None, None, None]
-     # value: 1
+   # [None, None, None, None]
+   # value: 1
 
-    def append(self, value):
-        if self.count == self.size:
-            self.resize()
+  def append(self, value):
+    if self.count == self.size:
+      self.resize()
 
-        self.storage[self.count] = value
-        self.count += 1
+    self.storage[self.count] = value
+    self.count += 1
 
-     # [1, 2, 2, 3, 4]
-    def insert(self, value, idx):
-        # resize if we need
-        if self.count == self.size:
-            self.resize()
+   # [1, 2, 2, 3, 4]
+  def insert(self, value, idx):
+    # resize if we need
+    if self.count == self.size:
+      self.resize()
 
-        # move everything over by one
-        for i in range(self.count, idx, -1):
-            self.storage[i] = self.storage[i - 1]
+    # move everything over by one
+    for i in range(self.count, idx, -1):
+      self.storage[i] = self.storage[i - 1]
 
-        self.storage[idx] = value
+    self.storage[idx] = value
 
-        self.count += 1
+    self.count += 1
 
-    def resize(self):
-        self.size = self.size * 2
+  def resize(self):
+    self.size = self.size * 2
 
-        # make a new array, which is double the size of the old array
-        # [None, None, None, None, None, None, None, None]
-        temp_storage = [None] * self.size
+    # make a new array, which is double the size of the old array
+    # [None, None, None, None, None, None, None, None]
+    temp_storage = [None] * self.size
 
-        # copy everything into the new array
-        for idx in range(self.size / 2): # can also use self.count
-            temp_storage[idx] = self.storage[idx]
+    # copy everything into the new array
+    for idx in range(self.size / 2): # can also use self.count
+      temp_storage[idx] = self.storage[idx]
 
-        self.storage = temp_storage
+    self.storage = temp_storage
