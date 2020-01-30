d6de525681735e261276369c138e09340b833f79
adam <adam.nawroc@gmail.com>
2020-01-29 22:53:12 -0500

    ok enough goofing off, starting hash func

--- src/hashtable.py
+++ src/hashtable.py
@@ -22,6 +22,13 @@ class HashTable:
 
     You may replace the Python hash with DJB2 as a stretch goal.
     '''
+
+    def hash(key):
+      total = 0
+      return len(key)
+
+    # return total % self.capacity
+
     return hash(key)
 
   def _hash_djb2(self, key):
@@ -81,27 +88,28 @@ class HashTable:
 if __name__ == "__main__":
   ht = HashTable(2)
 
-  ht.insert("line_1", "Tiny hash table")
-  ht.insert("line_2", "Filled beyond capacity")
-  ht.insert("line_3", "Linked list saves the day!")
+  print(ht._hash_mod('bob'))
+  # ht.insert("line_1", "Tiny hash table")
+  # ht.insert("line_2", "Filled beyond capacity")
+  # ht.insert("line_3", "Linked list saves the day!")
 
-  print("")
+  # print("")
 
-  # Test storing beyond capacity
-  print(ht.retrieve("line_1"))
-  print(ht.retrieve("line_2"))
-  print(ht.retrieve("line_3"))
+  # # Test storing beyond capacity
+  # print(ht.retrieve("line_1"))
+  # print(ht.retrieve("line_2"))
+  # print(ht.retrieve("line_3"))
 
-  # Test resizing
-  old_capacity = len(ht.storage)
-  ht.resize()
-  new_capacity = len(ht.storage)
+  # # Test resizing
+  # old_capacity = len(ht.storage)
+  # ht.resize()
+  # new_capacity = len(ht.storage)
 
-  print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+  # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-  # Test if data intact after resizing
-  print(ht.retrieve("line_1"))
-  print(ht.retrieve("line_2"))
-  print(ht.retrieve("line_3"))
+  # # Test if data intact after resizing
+  # print(ht.retrieve("line_1"))
+  # print(ht.retrieve("line_2"))
+  # print(ht.retrieve("line_3"))
 
-  print("")
+  # print("")
